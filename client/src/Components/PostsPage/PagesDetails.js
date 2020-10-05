e6add2d0d41982a464295db3bc341a2b78a5760b
shivakanyap <shivakanyapatil95@gmail.com>
2020-10-06 01:05:29 +0530

    initial

--- client/src/Components/PostsPage/PagesDetails.js
+++ client/src/Components/PostsPage/PagesDetails.js
@@ -1,6 +1,7 @@
+
 import React from 'react'
 import axios from 'axios'
-
+import PagesCard from './PagesCard'
 
 class PagesDetails extends React.Component{
     constructor(props){
@@ -12,10 +13,10 @@ class PagesDetails extends React.Component{
     }
     componentDidMount(){
         const id=this.props.match.params.id
-        
+        //console.log(id,'hhyy')
         axios.get(`https://jsonplaceholder.typicode.com/posts/${id}`)
         .then(response=>{
-           
+            console.log(response.data,'in res')
             this.setState(()=>({
                 posts:response.data
             }))
@@ -24,7 +25,7 @@ class PagesDetails extends React.Component{
 
         axios.get(`https://jsonplaceholder.typicode.com/comments`)
         .then(response=>{
-           
+            console.log(response.data,'in cccc')
             this.setState(()=>({
                 comments:response.data
             }))
@@ -33,13 +34,14 @@ class PagesDetails extends React.Component{
 
     }
     render(){
+        console.log(this.state.comments,'pics',this.props.match.params.id)
         return(
 
             <div>
                 <h1>Comments</h1>
-                { this.state.comments.filter(((item)=>item.postId ===this.props.match.params.id)).map(item=>{
+                { this.state.comments.filter(((item)=>item.postId == this.props.match.params.id)).map(item=>{
                     return (
-                        <p>*  {item.body}</p>
+                        <p>*{item.body}</p>
                     )
                    
                     })
@@ -54,4 +56,3 @@ class PagesDetails extends React.Component{
     }
 }
 export default PagesDetails
-
